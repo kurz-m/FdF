d51167ef8301224414b5ae62d9679ccc9e6f6881
Markus Kurz <makurz@2-E-2.42heilbronn.de>
2023-05-08 18:32:32 +0200

    ADD comment to map parsing

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: makurz <dumba@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 13:14:59 by makurz            #+#    #+#             */
-/*   Updated: 2023/05/08 17:20:40 by makurz           ###   ########.fr       */
+/*   Updated: 2023/05/08 18:32:27 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,7 @@ static void	get_points(t_fdf *fdf)
 		x = -1;
 		columns = ft_split((char *) rows->content, ' ');
 		while (++x < fdf->map.width)
+			//TODO: change values of y here
 			set_point(&fdf->map, x, fdf->map.height - y, ft_atoi(columns[x]));
 		++y;
 		rows = rows->next;
