4d9b4bdeb50d744b665a53fe4ab0ced0bbefc770
kurz-m <m.kurz92@gmail.com>
2023-05-24 00:57:26 +0200

    implement rotation matrix proper

--- src/rotate.c
+++ src/rotate.c
@@ -6,11 +6,81 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:53:37 by makurz            #+#    #+#             */
-/*   Updated: 2023/05/20 16:09:14 by makurz           ###   ########.fr       */
+/*   Updated: 2023/05/24 00:56:43 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+typedef struct s_rotation
+{
+	double	r[3][3];
+}	t_rotation;
+
+t_rotation	init_r_x(double angle)
+{
+	t_rotation	matrix;
+
+	matrix.r[0][0] = 1;
+	matrix.r[0][1] = 0;
+	matrix.r[0][2] = 0;
+	matrix.r[1][0] = 0;
+	matrix.r[1][1] = cos(angle);
+	matrix.r[1][2] = -sin(angle);
+	matrix.r[2][0] = 0;
+	matrix.r[2][1] = sin(angle);
+	matrix.r[2][2] = cos(angle);
+	return (matrix);
+}
+
+t_rotation	init_r_y(double angle)
+{
+	t_rotation	matrix;
+
+	matrix.r[0][0] = cos(angle);
+	matrix.r[0][1] = 0;
+	matrix.r[0][2] = sin(angle);
+	matrix.r[1][0] = 0;
+	matrix.r[1][1] = 1;
+	matrix.r[1][2] = 0;
+	matrix.r[2][0] = -sin(angle);
+	matrix.r[2][1] = 0;
+	matrix.r[2][2] = cos(angle);
+	return (matrix);
+}
+
+t_rotation	init_r_z(double angle)
+{
+	t_rotation	matrix;
+
+	matrix.r[0][0] = cos(angle);
+	matrix.r[0][1] = -sin(angle);
+	matrix.r[0][2] = 0;
+	matrix.r[1][0] = sin(angle);
+	matrix.r[1][1] = cos(angle);
+	matrix.r[1][2] = 0;
+	matrix.r[2][0] = 0;
+	matrix.r[2][1] = 0;
+	matrix.r[2][2] = 1;
+	return (matrix);
+}
+
+
+t_point3D	rotate_point3d(t_point3D point, t_rotation mat)
+{
+	t_point3D	result;
+
+	result.x = mat.r[0][0] * point.x + \
+			  mat.r[0][1] * point.y + \
+			  mat.r[0][2] * point.z;
+	result.y = mat.r[1][0] * point.x + \
+			  mat.r[1][1] * point.y + \
+			  mat.r[1][2] * point.z;
+	result.z = mat.r[2][0] * point.x + \
+			  mat.r[2][1] * point.y + \
+			  mat.r[2][2] * point.z;
+	return (result);
+}
 
 // function to rotate around the x-Axis of the image
 t_point3D	rotate_x(t_point3D point, double angle)
